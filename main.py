6b356a4c15c232df020e887d810ac24597d5d274
paul <paulvrobson@gmail.com>
2017-09-24 21:05:00 -0500

    added main and templates

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-from Flask import flask,
+from flask import Flask, request, redirect, render_template
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+
+@app.route("/")
+def index():
+    return render_template('signup-form.html')
+
+app.run()
